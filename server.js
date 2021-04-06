969881e0b28e3a8103cff990480dae2c330871c5
MTZGER <66328710+MTZGER@users.noreply.github.com>
2021-04-06 12:53:03 +0200

    deploy to gh-pages 17

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const port = 3000
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
-app.get('/', (req, res) => {
+app.get('/react_chat_odata/', (req, res) => {
   res.json('Hello World!')
 })
 
