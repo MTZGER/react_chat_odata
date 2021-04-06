1297a715f2112c09bfb0fde4129d993080224d16
MTZGER <66328710+MTZGER@users.noreply.github.com>
2021-04-06 11:48:08 +0200

    Updates

--- static/js/main.d912d486.chunk.js
+++ static/js/main.d912d486.chunk.js
@@ -1,2 +1,2 @@
 (this.webpackJsonpreact_chat_odata=this.webpackJsonpreact_chat_odata||[]).push([[0],{11:function(t,c,e){"use strict";e.r(c);var n=e(1),a=e.n(n),r=e(3),s=e.n(r),i=(e(8),e.p,e(9),e(0));var o=function(){return Object(i.jsx)("div",{className:"App",children:Object(i.jsx)("h1",{children:"App"})})};s.a.render(Object(i.jsx)(a.a.StrictMode,{children:Object(i.jsx)(o,{})}),document.getElementById("root"))},8:function(t,c,e){},9:function(t,c,e){}},[[11,1,2]]]);
-//# sourceMappingURL=main.43b965e0.chunk.js.map
+//# sourceMappingURL=main.d912d486.chunk.js.map
